3e9750caa2ce78db790ccdd48e3966b52c2c06b0
Diana Grams <deegrams221@gmail.com>
2019-09-19 15:48:40 -0600

    working on Update form

--- client/src/Movies/UpdateForm.js
+++ client/src/Movies/UpdateForm.js
@@ -12,12 +12,75 @@
 //            stars: ['Kurt Russell', 'Bill Paxton', 'Sam Elliot'],
 //           }
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-const UpdateForm = () => {
+const initialMovie = {
+  title: '',
+  director: '',
+  metascore: '',
+  stars: [],  
+};
+
+const UpdateForm = (props) => {
+  const [movie, setMovie] = useState(initialMovie);
+  
+  const {match, movies} = props;
+
+  useEffect(() => {
+    const id = match.params.id;
+
+    const movieToUpdate = movies.find(movie => `${movie.id}` === id);
+    if (movieToUpdate) {
+      console.log(`movieToUpdate`, movieToUpdate);
+      setMovie(movieToUpdate);
+    }
+  }, [match, movies]);
+
+  const changeHandler = e => {
+    e.persist();
+    setMovie({...movie, [e.target.name]: e.target.value});
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .put(`http://localhost:5000/api/movies/${movie.id}`, movie)
+      .then(res => {
+        console.log(res);
+        props.updateMovie(res.data);
+        props.history.push(`/movies`);
+        setMovie(initialMovie);
+      })
+      .catch(error => console.log(error.response))
+  };
+
+  const handleStar = index => e => {
+    setMovie({...movie, stars: movie.star.map((star, starIndex) => {
+      return starIndex === index ? e.target.value : star;
+    })});
+  };
+
+  const addStar = e => {
+    e.preventDefault();
+    setMovie({...movie, stars: [...movie.stars, '']});
+  };
+
   return (
     <div>
       <h1>Update Movie</h1>
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='title' onChange={changeHandler} placeholder='title' value={movie.title} />
+        <input type='text' name='director' onChange={changeHandler} placeholder='director' value={movie.director} />
+        <input type='number' name='metascore' onChange={changeHandler} placeholder='metascore' value={movie.metascore} />
+
+        {movie.stars.map((starName, index) => {
+          return <input type='text' placeholder='star' value={starName} key={index} OnChange={handleStar(index)} />
+        })}
+      <button onClick={addStar}>Add Star</button>
+
+      <button type='submit'>Update</button>
+      </form>
     </div>
   );
 };
